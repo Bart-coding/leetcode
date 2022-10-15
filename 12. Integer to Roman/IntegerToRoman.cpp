d30bbf6eb81a830e8d6d95dd421e657beff3703a
Bart-coding <bartekpoczta54@gmail.com>
2022-10-15 23:56:39 +0200

    Uploaded two own submitted solutions to two Leetcode problems

---
+++ 12. Integer to Roman/IntegerToRoman.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+        const char romanSymbols[4][2] =
+        {
+            {'I','V'}, //ones
+            {'X','L'}, //tens
+            {'C','D'}, //hundreds
+            {'M'} //thousands
+        };
+        
+        int i = 0; //to iterate through romanSymbols
+        int actualLastDigit;
+        string convertedNumber = "";
+        while (num!=0)
+        {
+            actualLastDigit = num%10;
+            num = num/10;
+            
+            if (actualLastDigit != 0)
+            {
+                if (actualLastDigit <= 3)
+                convertedNumber.insert(0,actualLastDigit,romanSymbols[i][0]);
+                else if (actualLastDigit == 4)
+                {
+                	//convertedNumber.insert(0,romanSymbols[i]); //-> b³¹d, bierze wszystkie romanSymbols
+                    convertedNumber.insert(0,1,romanSymbols[i][1]);
+                    convertedNumber.insert(0,1,romanSymbols[i][0]);
+                }
+                else if (actualLastDigit >= 5 && actualLastDigit < 9)
+                {
+                    if (actualLastDigit > 5)
+                        convertedNumber.insert(0,actualLastDigit-5,romanSymbols[i][0]);
+                    convertedNumber.insert(0,1,romanSymbols[i][1]);
+                }
+                else
+                {
+                    convertedNumber.insert(0,1,romanSymbols[i+1][0]);
+                    convertedNumber.insert(0,1,romanSymbols[i][0]);
+                }
+            }
+            
+            i++;
+        }
+        
+        return convertedNumber;
+    }
+};
