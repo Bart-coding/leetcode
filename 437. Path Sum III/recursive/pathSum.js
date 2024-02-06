2f0ff2325ecf3ff2dc20a4b26170fed75a6cd559
Bart-coding <bartekpoczta54@gmail.com>
2024-02-06 04:29:55 +0100

    Corrected name of variable

--- 437. Path Sum III/recursive/pathSum.js
+++ 437. Path Sum III/recursive/pathSum.js
@@ -1,10 +1,10 @@
 var pathSum = function (root, sum) {
     if (!root) return 0;
-    function pathSumFromCurrentRoot(currentRoot, missingVal) {
+    function pathSumFromCurrentRoot(currentNode, missingVal) {
         let currentSum = 0;
-        if (currentRoot.val === missingVal) ++currentSum;
-        if (currentRoot.left) currentSum += pathSumFromCurrentRoot(currentRoot.left, missingVal - currentRoot.val);
-        if (currentRoot.right) currentSum += pathSumFromCurrentRoot(currentRoot.right, missingVal - currentRoot.val);
+        if (currentNode.val === missingVal) ++currentSum;
+        if (currentNode.left) currentSum += pathSumFromCurrentRoot(currentNode.left, missingVal - currentNode.val);
+        if (currentNode.right) currentSum += pathSumFromCurrentRoot(currentNode.right, missingVal - currentNode.val);
         return currentSum;
     }
     return pathSumFromCurrentRoot(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
