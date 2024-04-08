0d437623868708f941a203bf1821296efc61d12a
Bart-coding <bartekpoczta54@gmail.com>
2024-04-08 22:50:21 +0200

    Binary Search solution for 162

---
+++ 162. Find Peak Element/README.md
@@ -0,0 +1,4 @@
+## Input:
+* integer array nums where nums[i] != nums[i+1]
+## Output:
+* any number greater than its two neighbors (or than one nehgbor for the first or last number of the array)
---
+++ 162. Find Peak Element/findPeakElement.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findPeakElement(self, nums: list[int]) -> int:
+        if len(nums) == 1:
+            return 0
+        if len(nums) == 2:
+            return nums.index(max(nums))
+        if nums[0] > nums[1]:
+            return 0
+        if nums[-1] > nums[-2]:
+            return len(nums) - 1
+        left = 1
+        right = len(nums) - 2
+        while left < right:
+            mid = (left + right) // 2
+            greater_than_prev = nums[mid] > nums[mid - 1]
+            greater_than_next = nums[mid] > nums[mid + 1]
+            if greater_than_prev and greater_than_next:
+                return mid
+            if not greater_than_prev:
+                right = mid - 1
+            elif not greater_than_next:
+                left = mid + 1
+        return left
