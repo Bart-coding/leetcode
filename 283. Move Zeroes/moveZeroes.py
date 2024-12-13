ea5e00f04b669ac0946e211a0e66b814f151ef87
Bart-coding <bartekpoczta54@gmail.com>
2024-12-13 23:29:53 +0100

    Upload solution for 283 (two-pointer)

---
+++ 283. Move Zeroes/moveZeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        first_zero_idx = -1
+        try:
+            first_zero_idx = nums.index(0)
+        except ValueError:
+            return
+        
+        for i in range(first_zero_idx + 1, len(nums)):
+            if nums[i] != 0:
+                nums[first_zero_idx] = nums[i]
+                nums[i] = 0
+                first_zero_idx += 1
