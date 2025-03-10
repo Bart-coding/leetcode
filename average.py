a5e7e1b91773d7cdb45bfbb255c9057329867d98
Bartłomiej Chilmon <74357483+Bart-coding@users.noreply.github.com>
2025-03-10 21:13:06 +0100

    Delete average.py

--- average.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def average(self, salary: List[int]) -> float:
-        
-        sum_without_extremes = sum(salary) - min(salary) - max(salary)
-
-        return sum_without_extremes / (len(salary) - 2)
